02caba603609da37da21fc62a535674543502ab3
David Wright <david.wright@bluewin.ch>
2016-05-17 17:28:04 +0200

    + Added FreeRTOS and created an example task.

--- main.cpp
+++ main.cpp
@@ -25,19 +25,28 @@
 #include "stdint.h"
 #include "Arduino.h"
 #include "Adafruit_NeoPixel.h"
-
+#include "FreeRTOS_AVR.h"
 /*----------------------------------------------------------------------------*/
 /* defines */
 /*----------------------------------------------------------------------------*/
-#define DF_NUM_LEDS 10U
+#define DF_NUM_LEDS_PER_STRIP 10U
 #define DF_PIN	6U
+#define DF_NUM_STRIPS 12
+
+/*----------------------------------------------------------------------------*/
+/* forward declarations */
+/*----------------------------------------------------------------------------*/
+void task(void* apvArgument);
+
 /*----------------------------------------------------------------------------*/
 /* globals */
 /*----------------------------------------------------------------------------*/
 /*! \brief WS2812 strip object */
-Adafruit_NeoPixel g_cPixels = Adafruit_NeoPixel(DF_NUM_LEDS, DF_PIN, NEO_GRB + NEO_KHZ800);
-/*! \brief Delay variable*/
+Adafruit_NeoPixel g_cPixels = Adafruit_NeoPixel(DF_NUM_LEDS_PER_STRIP, DF_PIN, NEO_GRB + NEO_KHZ800);
+/*! \brief Delay variable */
 uint16_t g_uDelayVal = 500U;
+/*! \brief task handles */
+TaskHandle_t g_pvTaskHandles[DF_NUM_STRIPS];
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -53,6 +62,9 @@ uint16_t g_uDelayVal = 500U;
 void setup(void)
 {
 	g_cPixels.begin();
+	xTaskCreate(task, "example task", 1024, 0, 1, g_pvTaskHandles);
+	vTaskStartScheduler();
+
 }
 
 /*----------------------------------------------------------------------------*/
@@ -68,7 +80,7 @@ void setup(void)
 /*----------------------------------------------------------------------------*/
 void loop(void)
 {
-  for(uint8_t uLoop = 0; uLoop < DF_NUM_LEDS; uLoop++){
+  for(uint8_t uLoop = 0; uLoop < DF_NUM_LEDS_PER_STRIP; uLoop++){
 	// pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
 	g_cPixels.setPixelColor(uLoop, g_cPixels.Color(0,150,0)); // Moderately bright green color.
 
@@ -77,4 +89,12 @@ void loop(void)
 	delay(g_uDelayVal); // Delay for a period of time (in milliseconds).
 
   }
+}
+
+void task(void* apvArgument)
+{
+	while(true)
+	{
+
+	}
 }
