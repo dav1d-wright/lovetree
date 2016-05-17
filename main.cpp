ab47c20c219b2381c97d9c13a412bb39fa87363c
David Wright <david.wright@bluewin.ch>
2016-05-17 14:10:27 +0200

    + Initial commit. ! Added FastLED and Adafruit_NeoPixel library, but I'm still having problems with FastLED, Adafruit_NeoPixel seems to work so use this one. + Added basic structure in main.cpp + Added Eclipse settings and a pdf as guideline.

---
+++ main.cpp
@@ -0,0 +1,80 @@
+/**----------------------------------------------------------------------------
+	\file		Main.cpp
+	\author		D. Wright
+	\version	0
+
+	\brief Main file
+
+----------------------------------------------------------------------------*/
+
+
+/*----------------------------------------------------------------------------*/
+
+/*!	\defgroup Main
+	\{
+*/
+
+/*!	\defgroup Main (MAIN)
+ 	 * \brief Main File
+	\{
+*/
+
+/*----------------------------------------------------------------------------*/
+/* includes */
+/*----------------------------------------------------------------------------*/
+#include "stdint.h"
+#include "Arduino.h"
+#include "Adafruit_NeoPixel.h"
+
+/*----------------------------------------------------------------------------*/
+/* defines */
+/*----------------------------------------------------------------------------*/
+#define DF_NUM_LEDS 10U
+#define DF_PIN	6U
+/*----------------------------------------------------------------------------*/
+/* globals */
+/*----------------------------------------------------------------------------*/
+/*! \brief WS2812 strip object */
+Adafruit_NeoPixel g_cPixels = Adafruit_NeoPixel(DF_NUM_LEDS, DF_PIN, NEO_GRB + NEO_KHZ800);
+/*! \brief Delay variable*/
+uint16_t g_uDelayVal = 500U;
+
+/*----------------------------------------------------------------------------*/
+/*!
+	\pre
+	None.
+
+	\post
+	Arduino is set up.
+
+	Arduino setup function.
+*/
+/*----------------------------------------------------------------------------*/
+void setup(void)
+{
+	g_cPixels.begin();
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+	\pre
+	setup() has been called.
+
+	\post
+	None.
+
+	Arduino loop function.
+*/
+/*----------------------------------------------------------------------------*/
+void loop(void)
+{
+  for(uint8_t uLoop = 0; uLoop < DF_NUM_LEDS; uLoop++){
+	// pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+	g_cPixels.setPixelColor(uLoop, g_cPixels.Color(0,150,0)); // Moderately bright green color.
+
+	g_cPixels.show(); // This sends the updated pixel color to the hardware.
+
+	delay(g_uDelayVal); // Delay for a period of time (in milliseconds).
+
+  }
+}
